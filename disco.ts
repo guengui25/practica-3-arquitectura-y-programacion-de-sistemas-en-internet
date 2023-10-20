38df81867fd724b3c17bfd1b73992529f856eba7
guengui25 <guengui25@hotmail.com>
2023-10-20 18:12:40 +0200

    Implementación mongo

--- disco.ts
+++ disco.ts
@@ -1,9 +1,31 @@
-export interface disco {
+//Creo el esquema para mongo y tipo para typescript
+
+import mongoose from "npm:mongoose@7.6.3"; //Importo mongoose
+
+const Schema = mongoose.Schema; //Creo un esquema
+const ObjectId = Schema.ObjectId; //Creo un id de esquema
+
+//Creo el tipo disco
+export interface DiscoModelType {
     nombre:string,
     autor:string,
     formato:string,
     matriz?:string,
     pais_impresion:string,
     arte_portada:string, //URL de la imagen
-    id:number
 }
+
+//Creo el esquema de disco con mongo
+const DiscoSchema = new Schema({
+    //Son en mayusculas porque son los tipos de mongoose
+    nombre:String,
+    autor:String,
+    formato:String,
+    matriz:String,
+    pais_impresion:String,
+    arte_portada:String, //URL de la imagen
+    //Mongo añade '_id' por defecto
+});
+
+//Exporto el modelo de persona
+export const DiscoModel = mongoose.model<DiscoModelType>("Disco", DiscoSchema);
--- main.ts
+++ main.ts
@@ -4,89 +4,63 @@
 
 import express , {Request, Response}from "npm:express@4.18.2"; //Importo express
 
-import {disco} from "./disco.ts"; //Importo desde definiciones disco
+//Deno deploy
+//https://docs.deno.com/deploy/manual
+
+
+//https://www.npmjs.com/package/mongoose
+//https://mongoosejs.com/docs/
+
+import mongoose from "npm:mongoose@7.6.3"; //Importo mongoose
+
+import {DiscoModel, DiscoModelType} from "./disco.ts"; //Importo desde definiciones disco
 
 const app = express(); //Inicializo el servidor de express
 
-const temporal:disco[] = [{
-    nombre: "Álbum 1",
-    autor: "Artista 1",
-    formato: "CD",
-    matriz: "MATRIZ_1",
-    pais_impresion: "País 1",
-    arte_portada: "URL_1",
-    id: 1
-  },
-  {
-    nombre: "Álbum 2",
-    autor: "Artista 2",
-    formato: "Vinilo",
-    pais_impresion: "País 2",
-    arte_portada: "URL_2",
-    id: 2
-  },
-  {
-    nombre: "Álbum 3",
-    autor: "Artista 3",
-    formato: "CD",
-    matriz: "MATRIZ_3",
-    pais_impresion: "País 3",
-    arte_portada: "URL_3",
-    id: 3
-  },
-  {
-    nombre: "Álbum 4",
-    autor: "Artista 4",
-    formato: "Casete",
-    pais_impresion: "País 4",
-    arte_portada: "URL_4",
-    id: 4
-  },
-  {
-    nombre: "Álbum 5",
-    autor: "Artista 5",
-    formato: "Vinilo",
-    matriz: "MATRIZ_5",
-    pais_impresion: "País 5",
-    arte_portada: "URL_5",
-    id: 5
-  }];
+//Middleware para analizar el cuerpo de la solicitud JSON
+//Hago que pueda recibir el body y sepa interpretarlo
+app.use(express.json());
+
+//Intento la conexión a la base de datos AtlasMongoDB usando Mongoose
+try {
+    await mongoose.connect("mongodb+srv://guengui25:1234@cluster0.ioi135h.mongodb.net/Practica3-Discos?retryWrites=true&w=majority");
+    console.log("Conexión exitosa a MongoDB");
+} catch (error) {
+    console.error("Error al conectar a MongoDB:", error);
+}
 
 //Métodos GET
 //Obtener todos los discos existentes
-app.get("/get/discos",(req: Request,res: Response)=>{
-    res.json(temporal);  //Mando el json con todos los discos
+app.get("/get/discos",async (req: Request,res: Response)=>{
+
+    const coleccion = await DiscoModel.find().exec();
+
+    res.json(coleccion);  //Mando el json con todos los discos
+
 });
 
 //Obtener un disco mediante id
-app.get("/get/disco/:id",(req: Request,res: Response)=>{
+app.get("/get/disco/:id",async (req: Request,res: Response)=>{
     try{
-        const id = Number(req.params.id);
-        if(isNaN(id)){
-            res.status(400).send("El id tiene que ser un número");
-            return;
-        }
+        const id = req.params.id;
+        const coincidencia_id = await DiscoModel.findById(id).exec();
 
-        temporal.forEach(element => {
-            if(element.id === id){
-                res.json(element); //Si el id dado es igual al id del disco devuelvo su información
-            }
-        })
+        res.json(coincidencia_id); //Mando el json con el disco o lo que sea que haya encontrado
     }
+
     catch(e){
         res.status(500).send("Error 500"); //Devuelvo un status 500 (error fatal)
     }
 });
 
 //Obtener listado de discos según nombre
-app.get("/get/discos/:nombre",(req: Request,res: Response)=>{
+app.get("/get/discos/:nombre",async (req: Request,res: Response)=>{
     try{
         const nombre = req.params.nombre;
-        const coincide = temporal.filter(element => {
-            return element.nombre.includes(nombre)
-        })
 
-        res.json(coincide); //Si cumple con el nombre
+        const listado_nombre = await DiscoModel.find({nombre: nombre}).exec();
+
+        res.json(listado_nombre); //Si cumple con el nombre
     }
     catch(e){
         res.status(500).send("Error 500"); //Devuelvo un status 500 (error fatal)
@@ -94,14 +68,13 @@ app.get("/get/discos/:nombre",(req: Request,res: Response)=>{
 });
 
 //Obtener listado de discos según formato
-app.get("/get/discos/listado/formato/:formato",(req: Request,res: Response)=>{
+app.get("/get/discos/listado/formato/:formato",async (req: Request,res: Response)=>{
     try{
         const formato = req.params.formato;
-        const coincide = temporal.filter(element => {
-            return element.formato === formato
-        })
+        
+        const listado_formato = await DiscoModel.find({formato: formato}).exec();
 
-        res.json(coincide); //Si cumple con el formato
+        res.json(listado_formato); //Si cumple con el formato
     }
     catch(e){
         res.status(500).send("Error 500"); //Devuelvo un status 500 (error fatal)
@@ -109,14 +82,13 @@ app.get("/get/discos/listado/formato/:formato",(req: Request,res: Response)=>{
 });
 
 //Obtener listado de discos según país de impresión
-app.get("/get/discos/listado/pais/:pais",(req: Request,res: Response)=>{
+app.get("/get/discos/listado/pais/:pais",async (req: Request,res: Response)=>{
     try{
         const pais = req.params.pais;
-        const coincide = temporal.filter(element => {
-            return element.pais_impresion.includes(pais);
-        })
+        
+        const listado_pais = await DiscoModel.find({pais_impresion: pais}).exec();
 
-        res.json(coincide); //Si cumple con el formato
+        res.json(listado_pais); //Si cumple con el formato
     }
     catch(e){
         res.status(500).send("Error 500"); //Devuelvo un status 500 (error fatal)
@@ -125,61 +97,70 @@ app.get("/get/discos/listado/pais/:pais",(req: Request,res: Response)=>{
 
 
 //Métodos POST
+app.post("/post",async (req: Request,res: Response)=>{
 
-//Middleware para analizar el cuerpo de la solicitud JSON
-//Hago que pueda recibir el body y sepa interpretarlo
-app.use(express.json());
-
-app.post("/post",(req: Request,res: Response)=>{
-
-    const disco = req.body; //Especifico que el disco tiene la información en el body del request
+    const disco:DiscoModelType = req.body; //Especifico que el disco tiene la información en el body del request
 
     console.log(disco); //Muestro por consola la información del disco a añadir
 
-    disco.id = temporal.length + 1; //Agrego el id -- Cambiar por el de mongo
+        //Verifico que el disco está completo
+        if (!disco.nombre || !disco.autor || !disco.formato || !disco.matriz || !disco.pais_impresion || !disco.arte_portada) {
+            res.status(403).send("Error, faltan datos");
+            return;
+        }
+    
+        //Compruebo si hay algún disco con el mismo nombre y autor
+        const existe = await DiscoModel.findOne({nombre: disco.nombre,autor: disco.autor}).exec();
+    
+        if(existe){
+            res.status(403).send("Error, ya existe una disco con ese nombre y autor");
+            return;
+        }
+    
+        const nuevoDisco = await DiscoModel.create({nombre: disco.nombre,autor: disco.autor,
+            formato: disco.formato,matriz: disco.matriz,pais_impresion: disco.pais_impresion,
+            arte_portada: disco.arte_portada});
 
-    temporal.push(disco); //Agrego el disco a la "base de datos"
-
-    res.json({ mensaje: 'Datos recibidos correctamente', disco}); //Devuelvo un JSON con el disco, para mostrarlo
+    res.send("Datos recibidos correctamente"); //Devuelvo un mensaje de que se ha recibido correctamente
 });
 
 //Métodos PUT
-app.put("/put",(req: Request,res: Response)=>{
+app.put("/put",async (req: Request,res: Response)=>{
+    try {
+        const disco = req.body; //Especifico que el disco tiene la información en el body del request
 
-    const disco = req.body; //Especifico que el disco tiene la información en el body del request
-
-    console.log(disco); //Muestro por consola la información del disco a añadir
-
-    temporal.forEach(element => {
-        if(element.id === disco.id){
-            element = disco;
+        if (!disco._id || !disco.nombre || !disco.autor || !disco.formato || !disco.matriz || !disco.pais_impresion || !disco.arte_portada) {
+            res.status(403).send("Error, faltan datos");
+            return;
         }
-    });
 
-    res.json({ mensaje: 'Datos modificados correctamente', disco});
+        const actualizar_disco = await DiscoModel.findByIdAndUpdate(disco._id,disco).exec(); 
+
+        if(!actualizar_disco) res.send("No se ha encontrado el disco a modificar")
+        else res.send("Datos modificados correctamente");
+
+    } catch (error) {
+        res.status(500).send("Error 500"); //Devuelvo un status 500 (error fatal)
+    }
 });
 
 //Métodos DELETE
-app.delete("/delete/:id",(req: Request,res: Response)=>{
+app.delete("/delete/:id",async (req: Request,res: Response)=>{
 
     try{
-        const id = Number(req.params.id);
-        if(isNaN(id)){
-            res.status(400).send("El id tiene que ser un número");
-            return;
-        }
-        console.log("Se va a borrar el disco con el id:",id); //Muestro por consola la información del disco a añadir
+        const id = req.params.id; //Especifico que el disco tiene la información en el body del request
 
-        temporal.splice(id-1,1) //Elimino el elemento con el id dado
+        const coincidencia_id = await DiscoModel.findByIdAndDelete(id).exec(); //Delete es el apropiado, REMOVE está obsoleto
 
-        res.json({ mensaje: "El disco con el id "+id+" se ha borrado correctamente"});
+        if(!coincidencia_id) res.send("No se ha encontrado el disco a borrar")
+        else res.send("El disco con el id "+id+" se ha borrado correctamente"); //Devuelvo un mensaje de que se ha borrado correctamente
     }
     catch(e){
         res.status(500).send("Error 500"); //Devuelvo un status 500 (error fatal)
     } 
 });
 
-
+//=======================================================================================================
 //Ejecución
 app.listen(3000, () => {
     console.log("El servidor está corriendo en el puerto 3000")
